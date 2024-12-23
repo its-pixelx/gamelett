3c84d2b58188d906ea7419579694980c3a663a50
PixelTechi <obsidiandeveloper6@gmail.com>
2024-12-23 16:19:20 +0530

    Delete download.js

--- download.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const downloadUrl = urlParams.get("downloadUrl");
-    const downloadLink = document.getElementById("download-link");
-
-    if (downloadUrl && downloadLink) {
-        downloadLink.href = downloadUrl;
-    }
-});
